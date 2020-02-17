94c23a0eb6612454b61dd61d6dfba04209982e7a
eyupams <43216769+eyupams@users.noreply.github.com>
2020-02-17 22:46:28 +0100

    Fixed message checking on multiple words

--- chatclient/UDPclient.py
+++ chatclient/UDPclient.py
@@ -19,31 +19,33 @@ def getData():
         data_string = data.decode("utf-8")
         data_list = data_string.split()
 
-        if len(data_list) == 3 and data_list[0] != "WHO-OK":
+        if data_list[0] == "DELIVERY":
 
-            #Send 100 if you want to test this, replace acmessage with one of these.
             message1bitFlipped = b"101"
             message2bitFlipped = b"111"
             message3bitFlipped = b"011"
-            acmessage = data_list[2].split('{')[0]
-            lrcRecieved = data_list[2].split('{')[1]
+            
+            message = ""
+            #https://stackoverflow.com/questions/6148619/start-index-for-iterating-python-list
+            for x in data_list[2:]:
+                message += x + " "
+            acmessage = message.split('{')[0]
+            lrcRecieved = message.split('{')[1]
 
             print("MESSAGE INTO BYTEARRAY: " + acmessage)
             dataArray = bytearray(acmessage.encode())
             
             #https://en.wikipedia.org/wiki/Checksum
-            #https://stackoverflow.com/questions/29569791/incorrect-lrc-value-calculated-from-checksum
             lrc = 0
             for b in dataArray:
                 lrc ^= b
             print("checksum of recieved message: " + str(lrc))
             
-            print("checsum of ARRIVED sent message: " + str(lrcRecieved))
-           
-            if (str(lrc) == str(lrcRecieved)):
-               print("No odd-bit error, but you might want to check for a double digit error")
+            print("checksum of ARRIVED sent message: " + str(lrcRecieved))
+            if (lrc == int(lrcRecieved)):
+               print("No odd-bit error, but you might want to check for a double digit error\n")
             else:
-                print("Yeah you have a odd-bit error")
+                print("Yeah you have a odd-bit error\n")
             
 
 
@@ -57,8 +59,11 @@ def getData():
 
         elif data_list[0] == "DELIVERY":
             username = data_list[1]
-            acmessage = data_list[2].split('{')[0]
-            print("[" + username + " -> me] " + acmessage)
+            message = ""
+            #https://stackoverflow.com/questions/6148619/start-index-for-iterating-python-list
+            for x in data_list[2:]:
+                message += x + " "
+            print("[" + username + " -> me] " + message.split('{')[0])
 
         elif data_list[0] == "BUSY":
             print("Server is full, fuck off.\n")
